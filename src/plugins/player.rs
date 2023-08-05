1b169a3e9c5140e20be99fa65823ee74fe440d43
Rafael Ignacio Pinto <ignacio.rafael@gmail.com>
2023-08-05 18:32:54 -0300

    moving sprites to resource

--- src/plugins/player.rs
+++ src/plugins/player.rs
@@ -10,6 +10,12 @@ pub enum FacingDirection {
     Right,
 }
 
+#[derive(Resource)]
+pub struct PlayerSprites {
+    facing_up: Handle<Image>,
+    facing_down: Handle<Image>,
+}
+
 #[derive(Component, Default)]
 #[cfg_attr(feature = "debug", derive(Reflect), reflect(Component))]
 pub struct Player {
@@ -28,7 +34,8 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player)
+        app.add_systems(PreStartup, load_player_sprites)
+            .add_systems(Startup, spawn_player)
             .add_systems(Update, move_player)
             .add_systems(Update, update_player_sprite);
         #[cfg(feature = "debug")]
@@ -36,10 +43,17 @@ impl Plugin for PlayerPlugin {
     }
 }
 
-fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn load_player_sprites(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.insert_resource(PlayerSprites {
+        facing_down: asset_server.load("sprites/character_maleAdventurer_idle.png"),
+        facing_up: asset_server.load("sprites/character_maleAdventurer_back.png"),
+    })
+}
+
+fn spawn_player(mut commands: Commands, player_sprites: Res<PlayerSprites>) {
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("sprites/character_maleAdventurer_idle.png"),
+            texture: player_sprites.facing_down.clone(),
             ..default()
         },
         Player {
@@ -63,15 +77,15 @@ fn move_player(mut player_query: Query<&mut Player>, input: Res<Input<KeyCode>>)
 
 fn update_player_sprite(
     mut player_query: Query<(&mut Handle<Image>, &Player), With<Player>>,
-    asset_server: Res<AssetServer>,
+    player_sprites: Res<PlayerSprites>,
 ) {
     let (mut texture, player) = player_query.single_mut();
     match player.facing_direction {
         FacingDirection::Down => {
-            *texture = asset_server.load("sprites/character_maleAdventurer_idle.png");
+            *texture = player_sprites.facing_down.clone();
         }
         FacingDirection::Up => {
-            *texture = asset_server.load("sprites/character_maleAdventurer_back.png");
+            *texture = player_sprites.facing_up.clone();
         }
         FacingDirection::Left => todo!(),
         FacingDirection::Right => todo!(),
